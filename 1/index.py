268fb7f0463fc79e24ca6ad82618b32544c2df6e
Achi Chen <achichen.tw@gmail.com>
2013-07-27 21:59:45 +0800

    no message

--- 1/index.py
+++ 1/index.py
@@ -2,13 +2,34 @@ import tornado.wsgi
 
 from crawler import CrawlerHandler, CrawlerCallbackHandler
  
+import dao
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world! - Tornado\n")        
 
- 
+class ApiHandler(tornado.web.RequestHandler):
+    def get(self):
+        lat1 = self.get_argument("lat1", None)
+        lng1 = self.get_argument("lng1", None)
+        lat2 = self.get_argument("lat2", None)
+        lng2 = self.get_argument("lng2", None)
+
+        def _func(db):
+            criteria = None
+            if lat1 is not None and lng1 is not None and lat2 is not None and lng2 is not None:
+                criteria = {"$within":{"$box":[[float(lng1), float(lat1)], [float(lng2), float(lat2)]]}}
+
+            news_list = db.news.find(criteria)
+
+            self.write(news_list)
+
+        dao.db_action(_func)
+
+
 app = tornado.wsgi.WSGIApplication([
     (r"/", MainHandler),
+    (r"/api", ApiHandler),
     (r"/crawler/callback", CrawlerCallbackHandler),
     (r"/crawler", CrawlerHandler)
 ])
