d0d28b6cb949a96da1444686a8d667f9705a12ca
Achi Chen <achichen.tw@gmail.com>
2013-07-27 13:59:43 +0800

    Add a crawler handler

---
+++ 1/crawler.py
@@ -0,0 +1,7 @@
+
+class CrawlerHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.post()
+
+    def post(self):
+        self.write("Crawl!!")
--- 1/index.py
+++ 1/index.py
@@ -1,11 +1,15 @@
 import tornado.wsgi
+
+from crawler import CrawlerHandler
  
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world! - Tornado\n")        
+        
  
 app = tornado.wsgi.WSGIApplication([
     (r"/", MainHandler),
+    (r"/crawler", CrawlerHandler)
 ])
  
 from bae.core.wsgi import WSGIApplication
