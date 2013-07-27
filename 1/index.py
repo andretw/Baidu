157a5370e1bb1ef3e0cafd2dc29fa2e9836a55ee
baeadmin <baeadmin@baidu.com>
2013-07-27 23:27:53 +0800

    commit by edit_adapter

--- 1/index.py
+++ 1/index.py
@@ -34,8 +34,6 @@ class ApiHandler(tornado.web.RequestHandler):
                         }
                     }
                 }
-            
-            criteria = {}
 
             cursor = db.news.find()
         
