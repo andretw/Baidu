23481c0b60db08e553ee1c1b6eaba44f0a28f33e
Achi Chen <achichen.tw@gmail.com>
2013-07-27 14:55:41 +0800

    Fix

--- 1/crawler.py
+++ 1/crawler.py
@@ -1,21 +1,23 @@
+from bae.core import const
+from bae.api import logging
+
 import tornado.web
 import feedparser
-
-from bae.core import const
 import pymongo
 
 BAIDU_SEARCH_URL = "http://www.baidu.com/baidu"
 
 def crawl_news():
-    ret = feedparser.parse("http://news.baidu.com/ns?word=%CE%DB%C8%BE&tn=newsrss&sr=0&cl=2&rn=20&ct=0") 
-
-    db_name = 'ecomap'
-    con = pymongo.Connection(host = const.MONGO_HOST, port = int(const.MONGO_PORT))
-    db = con[db_name]
-    db.authenticate(const.MONGO_USER, const.MONGO_PASS)
-    news = db.news
-
+    con = None
     try:
+        ret = feedparser.parse("http://news.baidu.com/ns?word=%CE%DB%C8%BE&tn=newsrss&sr=0&cl=2&rn=20&ct=0") 
+
+        db_name = 'ecomap'
+        con = pymongo.Connection(host = const.MONGO_HOST, port = int(const.MONGO_PORT))
+        db = con[db_name]
+        db.authenticate(const.MONGO_USER, const.MONGO_PASS)
+        news = db.news
+
         # print ret.feed    
         for entry in ret.entries:
             print 20*"-"
@@ -28,8 +30,11 @@ def crawl_news():
                 "link": entry.link,
                 "description": entry.description
             })
+    except:
+        logging.exception('ERROR when crawl')
     finally:
-        con.disconnect()
+        if con:
+            con.disconnect()
 
 class CrawlerHandler(tornado.web.RequestHandler):
     def get(self):
