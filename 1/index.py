2f459a0fd7e7d078315cfb5198167cfcb7078f4b
Achi Chen <achichen.tw@gmail.com>
2013-07-27 22:09:35 +0800

    no message

--- 1/index.py
+++ 1/index.py
@@ -22,7 +22,16 @@ class ApiHandler(tornado.web.RequestHandler):
         def _func(db):
             criteria = None
             if lat1 is not None and lng1 is not None and lat2 is not None and lng2 is not None:
-                criteria = {"$within":{"$box":[[float(lng1), float(lat1)], [float(lng2), float(lat2)]]}}
+                criteria = {
+                    "loc": {
+                        "$within": {
+                            "$box":[
+                                [float(lng1), float(lat1)], 
+                                [float(lng2), float(lat2)]
+                            ]
+                        }
+                    }
+                }
 
             cursor = db.news.find(criteria)
         
